72233fee81436cbfbf1f26b8544e55c7d22a90c4
Filipe T. de Carvalho <fcarvalho@viceri.com.br>
2025-07-08 15:03:22 -0300

    feat: added sentry

--- index.tsx
+++ index.tsx
@@ -1,13 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
+import App from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+Sentry.init({
+  dsn: "https://9e36fe0e2e67486de76c4386d695a493@o4509633751875584.ingest.us.sentry.io/4509633754693632",
+  // Setting this option to true will send default PII data to Sentry.
+  // For example, automatic IP address collection on events
+  sendDefaultPii: true,
+});
 
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
-
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
