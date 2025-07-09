8713b4bb10effdb98739f25c26fc3913f084651b
Filipe T. de Carvalho <fcarvalho@viceri.com.br>
2025-07-09 16:01:37 -0300

    feat: added sentry

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig, loadEnv } from "vite";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, ".", "");
   return {
+    build: {
+      sourcemap: true,
+    },
     plugins: [
       react(),
+      sentryVitePlugin({
+        authToken: env.SENTRY_AUTH_TOKEN,
+        org: "carvalho-sistemas",
+        project: "impulsa",
+        telemetry: false,
+      }),
     ],
     define: {
       GEMINI_API_KEY: JSON.stringify(env.GEMINI_API_KEY),
